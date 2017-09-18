c4ed6615a1a3c6ecfa9a42d17c5f551b2f8e75b2
Milan Gallo <gallo.milan.jr@gmail.com>
2017-09-18 19:01:49 +0200

    db_dump
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

--- external/pgsql/db_dump.sql
+++ external/pgsql/db_dump.sql
@@ -1,68 +1,3 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 9.5beta2
--- Dumped by pg_dump version 9.5beta2
-
--- Started on 2017-08-16 22:19:58
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- TOC entry 2188 (class 1262 OID 16435)
--- Name: o2-carpool-develop; Type: DATABASE; Schema: -; Owner: o2-carpool
---
-
-CREATE DATABASE "o2-carpool-develop" WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'English_United States.1252' LC_CTYPE = 'English_United States.1252';
-
-
-ALTER DATABASE "o2-carpool-develop" OWNER TO "o2-carpool";
-
-\connect "o2-carpool-develop"
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- TOC entry 194 (class 3079 OID 12355)
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- TOC entry 2191 (class 0 OID 0)
--- Dependencies: 194
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET search_path = public, pg_catalog;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- TOC entry 183 (class 1259 OID 16469)
--- Name: city; Type: TABLE; Schema: public; Owner: o2-carpool
---
-
 CREATE TABLE city (
     id integer NOT NULL,
     name character varying(32) NOT NULL,
@@ -72,7 +7,7 @@ CREATE TABLE city (
 );
 
 
-ALTER TABLE city OWNER TO "o2-carpool";
+ALTER TABLE city OWNER TO "o2carpool";
 
 --
 -- TOC entry 182 (class 1259 OID 16467)
@@ -87,7 +22,7 @@ CREATE SEQUENCE city_id_seq
     CACHE 1;
 
 
-ALTER TABLE city_id_seq OWNER TO "o2-carpool";
+ALTER TABLE city_id_seq OWNER TO "o2carpool";
 
 --
 -- TOC entry 2192 (class 0 OID 0)
@@ -110,7 +45,7 @@ CREATE TABLE customer (
 );
 
 
-ALTER TABLE customer OWNER TO "o2-carpool";
+ALTER TABLE customer OWNER TO "o2carpool";
 
 --
 -- TOC entry 184 (class 1259 OID 16476)
@@ -125,7 +60,7 @@ CREATE SEQUENCE customer_id_seq
     CACHE 1;
 
 
-ALTER TABLE customer_id_seq OWNER TO "o2-carpool";
+ALTER TABLE customer_id_seq OWNER TO "o2carpool";
 
 --
 -- TOC entry 2193 (class 0 OID 0)
@@ -153,7 +88,7 @@ CREATE TABLE travel (
 );
 
 
-ALTER TABLE travel OWNER TO "o2-carpool";
+ALTER TABLE travel OWNER TO "o2carpool";
 
 --
 -- TOC entry 193 (class 1259 OID 16541)
@@ -168,7 +103,7 @@ CREATE TABLE travel_cities (
 );
 
 
-ALTER TABLE travel_cities OWNER TO "o2-carpool";
+ALTER TABLE travel_cities OWNER TO "o2carpool";
 
 --
 -- TOC entry 192 (class 1259 OID 16539)
@@ -183,7 +118,7 @@ CREATE SEQUENCE travel_cities_id_seq
     CACHE 1;
 
 
-ALTER TABLE travel_cities_id_seq OWNER TO "o2-carpool";
+ALTER TABLE travel_cities_id_seq OWNER TO "o2carpool";
 
 --
 -- TOC entry 2194 (class 0 OID 0)
@@ -207,7 +142,7 @@ CREATE SEQUENCE travel_id_seq
     CACHE 1;
 
 
-ALTER TABLE travel_id_seq OWNER TO "o2-carpool";
+ALTER TABLE travel_id_seq OWNER TO "o2carpool";
 
 --
 -- TOC entry 2195 (class 0 OID 0)
@@ -230,7 +165,7 @@ CREATE TABLE travel_type (
 );
 
 
-ALTER TABLE travel_type OWNER TO "o2-carpool";
+ALTER TABLE travel_type OWNER TO "o2carpool";
 
 --
 -- TOC entry 186 (class 1259 OID 16485)
@@ -245,7 +180,7 @@ CREATE SEQUENCE travel_type_id_seq
     CACHE 1;
 
 
-ALTER TABLE travel_type_id_seq OWNER TO "o2-carpool";
+ALTER TABLE travel_type_id_seq OWNER TO "o2carpool";
 
 --
 -- TOC entry 2196 (class 0 OID 0)
@@ -269,7 +204,7 @@ CREATE TABLE trip (
 );
 
 
-ALTER TABLE trip OWNER TO "o2-carpool";
+ALTER TABLE trip OWNER TO "o2carpool";
 
 --
 -- TOC entry 190 (class 1259 OID 16518)
@@ -284,7 +219,7 @@ CREATE SEQUENCE trip_id_seq
     CACHE 1;
 
 
-ALTER TABLE trip_id_seq OWNER TO "o2-carpool";
+ALTER TABLE trip_id_seq OWNER TO "o2carpool";
 
 --
 -- TOC entry 2197 (class 0 OID 0)
@@ -312,7 +247,7 @@ CREATE TABLE "user" (
 );
 
 
-ALTER TABLE "user" OWNER TO "o2-carpool";
+ALTER TABLE "user" OWNER TO "o2carpool";
 
 --
 -- TOC entry 180 (class 1259 OID 16455)
@@ -327,7 +262,7 @@ CREATE SEQUENCE user_id_seq
     CACHE 1;
 
 
-ALTER TABLE user_id_seq OWNER TO "o2-carpool";
+ALTER TABLE user_id_seq OWNER TO "o2carpool";
 
 --
 -- TOC entry 2198 (class 0 OID 0)
@@ -469,9 +404,9 @@ SELECT pg_catalog.setval('travel_id_seq', 1, false);
 --
 
 INSERT INTO travel_type VALUES (1, 'passenger', false);
-INSERT INTO travel_type VALUES (2, 'car_compan', false);
-INSERT INTO travel_type VALUES (3, 'car_rental', false);
-INSERT INTO travel_type VALUES (4, 'car_person', false);
+INSERT INTO travel_type VALUES (2, 'car_compan', true);
+INSERT INTO travel_type VALUES (3, 'car_rental', true);
+INSERT INTO travel_type VALUES (4, 'car_person', true);
 INSERT INTO travel_type VALUES (5, 'other', false);
 
 
